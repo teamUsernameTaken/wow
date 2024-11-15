ad16123a54cca33b2108f698fbaaca5297aae590
dysonsphere42 <dysonsphere042@proton.me>
2024-11-14 18:12:57 -0800

    added sshd config to v2

--- commencementv2.sh
+++ commencementv2.sh
@@ -1,10 +1,94 @@
 os="undeclared"
 
 commencementUbuntu(){
+  # Confirm with the user that the sources are correct
   echo "Please ensure the /etc/apt/sources.list file is correct"
   read -p "Press Enter to continue"
+  # Update everything
   apt update -y
   apt upgrade -y
+
+  # Detect if snap is installed
+  if command -v snap &>/dev/null; then
+    echo "Detected snap!"
+    # Update snap store
+    snap refresh
+    echo "Updated snap!"
+  else
+    echo "Did not detect snap skipping snap updates..."
+  fi
+
+  # Install required packages
+  apt install unattended-upgrades -y
+  apt install apt-listchanges -y
+  apt install bsd-mailx -y
+  apt install ufw -y
+  
+  # Enable unattended upgrades
+  dpkg-reconfigure -plow unattended-upgrades
+
+  # Enable and configure ufw
+  ufw enable
+  ufw default deny incomming
+  ufw default allow outgoing
+
+  # Check to see if sshd is installed
+  if command -v sshd &>/dev/null; then
+    echo "Detected sshd!"
+
+    editSshdConfig(){
+      local sshdConfig="/etc/ssh/sshd_config"
+      local param="$1"
+      local value="$2"
+
+      # Check if the param already exists in the file
+      if grep -q "^#*$param" "$sshdConfig"; then
+        # Update the param
+        sed -i "s/^#*\($param\s*\).*$/\1$value/" "$sshdConfig"
+      else
+        # Add the param
+        echo "$param $value" | tee -a "$sshdConfig" > /dev/null
+    }
+    
+    cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+
+    editSshdConfig "PermitRootLogin" "no"
+    editSshdConfig "MaxAuthTries" "3"
+    editSshdConfig "LoginGraceTime" "20"
+    editSshdConfig "PermitEmptyPasswords" "no"
+    editSshdConfig "ChallengeResponseAuthentication" "no"
+    editSshdConfig "KerberosAuthentication" "no"
+    editSshdConfig "GSSAPIAuthentication" "no"
+    editSshdConfig "X11Forwarding" "no"
+    editSshdConfig "PermitUserEnviroment" "no"
+    editSshdConfig "AllowAgentForwarding" "no"
+    editSshdConfig "AllowTcpForwarding" "no"
+    editSshdConfig "PermitTunnel" "no"
+    editSshdConfig "DebianBanner" "no"
+
+  fi
+
+
+  # Ask to reboot now or later
+  notify-send "Commencement V2: Completed updates please confirm reboot"
+  while true; do
+    read -p "Reboot now to apply kernal updates? (y/n): " choice
+    case "$choice" in
+      [Yy] )
+        echo "Rebooting now..."
+        reboot
+        break
+        ;;
+      [Nn] )
+        echo "Skipping reboot, please reboot at the end..."
+        sleep 1
+        break
+        ;;
+      * )
+        echo "Invalid option!"
+        ;;
+    esac
+  done
 }
 
 commencementFedora(){
